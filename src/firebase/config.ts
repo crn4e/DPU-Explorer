70b40ed2d984a3c3da5f98ac4846f92c7bb8c90e
Sitthichok Morpat <67111228@dpu.ac.th>
2025-12-01 10:23:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "dpu-explorer",
+  "appId": "1:706173066492:web:0d701bbd2844a2c89e0b79",
+  "apiKey": "AIzaSyDVZTfHCjrUgnHkGxF5NnFQINbX2j5v02Q",
+  "authDomain": "dpu-explorer.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "706173066492"
+};
